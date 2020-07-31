8a14b807865fa003112590113f7304ad17f988a4
Atte H <>
2020-07-31 21:43:59 +0300

    Cleaned up client

--- client.py
+++ client.py
@@ -4,7 +4,50 @@ from re import match
 
 API_URL = "http://localhost:5000"
 
-def prompt_from_schema(s, ctrl):
+def prompt_from_body(body):
+    """
+    """
+    item_choices = 0
+    try:
+        body["items"]
+    except KeyError:
+        print("{:-^80}".format(" Item "))
+        props = body.copy()
+        del props["@namespaces"]
+        del props["@controls"]
+        print(props)
+    else:
+        print("{:-^80}".format(" Collection "))
+        for item in body["items"]:
+            item_choices += 1
+            item_props = item.copy()
+            del item_props["@controls"]
+            print(item_choices, item_props)
+
+    choices = item_choices
+    print("{:-^80}".format(" Actions "))
+    for ctrl in body["@controls"]:
+        choices += 1
+        print(choices, ctrl)
+
+    pick = 0
+    while pick < 1 or pick > choices:
+        print("{:-^80}".format(" Prompt "))
+        pick = input("Pick " + ("an item or " if item_choices else "") + "an action (number): ")
+
+        try:
+            pick = int(pick)
+        except ValueError:
+            pick = 0
+    
+    if pick > item_choices:
+        ctrl = list(body["@controls"].values())[pick - item_choices - 1]
+    else:
+        ctrl = body["items"][pick - 1]["@controls"]["self"]
+
+    return ctrl
+
+def prompt_from_schema(ctrl):
     """
     """
     schema = ctrl["schema"]
@@ -51,104 +94,72 @@ def submit_data(s, ctrl, data):
         ctrl["method"],
         API_URL + ctrl["href"],
         data=json.dumps(data),
-        headers = {"Content-type": "application/json"}
+        headers={"Content-type": "application/json"}
     )
     return resp
 
-if __name__ == "__main__":
-    href = prev_href = "/api/"
-    while True:
-        with requests.Session() as s:
-            #TODO split this into a few functions and write docstrings
+def handle_action(s, ctrl):
+    """
+    """
+    try:
+        ctrl["schema"]
+    except KeyError:
+        pass
+    else:
+        req_ctrl, req_data = prompt_from_schema(ctrl)
+        resp = submit_data(s, req_ctrl, req_data)
+        try:
+            resp.raise_for_status()
+        except requests.HTTPError:
+            body = resp.json()
+            print("{:*^80}".format(" Error response "))
+            print(*body["@error"]["@messages"])
+        else:
+            print("{:*^80}".format(" Ok response "))
 
-            resp = s.get(API_URL + href)
+    try:
+        ctrl["method"]
+    except KeyError:
+        pass
+    else:
+        if ctrl["method"].lower() == "delete":
+            resp = s.delete(API_URL + ctrl["href"])
             try:
                 resp.raise_for_status()
             except requests.HTTPError:
-                href = prev_href
-                continue
-
-            print("{:=^80}".format(" " + href + " "))
-
-            try:
                 body = resp.json()
-            except ValueError:
-                print("{:*^80}".format(" Text response "))
-                print(resp.text)
-                href = prev_href
-                continue
-
-            item_choices = 0
-            try:
-                body["items"]
-            except KeyError:
-                print("{:-^80}".format(" Item "))
-                props = body.copy()
-                del props["@namespaces"]
-                del props["@controls"]
-                print(props)
+                print("{:*^80}".format(" Error response "))
+                print(*body["@error"]["@messages"])
             else:
-                print("{:-^80}".format(" Collection "))
-                for item in body["items"]:
-                    item_choices += 1
-                    item_props = item.copy()
-                    del item_props["@controls"]
-                    print(item_choices, item_props)
+                print("{:*^80}".format(" Ok response "))
 
-            choices = item_choices
-            print("{:-^80}".format(" Actions "))
-            for ctrl in body["@controls"]:
-                choices += 1
-                print(choices, ctrl)
-
-            pick = 0
-            while pick < 1 or pick > choices:
-                print("{:-^80}".format(" Prompt "))
-                subprompt = "an item or action" if item_choices else "an action"
-                pick = input("Pick {} (number): ".format(subprompt))
-
-                try:
-                    pick = int(pick)
-                except ValueError:
-                    pick = 0
-            
-            if pick > item_choices:
-                ctrl = list(body["@controls"].values())[pick - item_choices - 1]
-            else:
-                ctrl = body["items"][pick - 1]["@controls"]["self"]
-
-            try:
-                ctrl["schema"]
-            except KeyError:
-                pass
-            else:
-                req_ctrl, req_data = prompt_from_schema(s, ctrl)
-                resp = submit_data(s, req_ctrl, req_data)
+if __name__ == "__main__":
+    try:
+        with requests.Session() as s:
+            href = prev_href = "/api/"
+            while True:
+                resp = s.get(API_URL + href)
                 try:
                     resp.raise_for_status()
                 except requests.HTTPError:
+                    href = prev_href
+                    continue
+
+                print("{:=^80}".format(" " + href + " "))
+
+                try:
                     body = resp.json()
-                    print("{:*^80}".format(" Error response "))
-                    print(*body["@error"]["@messages"])
-                else:
-                    print("{:*^80}".format(" Ok response "))
+                except ValueError:
+                    print("{:*^80}".format(" Text response "))
+                    print(resp.text)
+                    href = prev_href
+                    continue
 
-            try:
-                ctrl["method"]
-            except KeyError:
-                pass
-            else:
-                if ctrl["method"].lower() == "delete":
-                    resp = s.delete(API_URL + ctrl["href"])
-                    try:
-                        resp.raise_for_status()
-                    except requests.HTTPError:
-                        body = resp.json()
-                        print("{:*^80}".format(" Error response "))
-                        print(*body["@error"]["@messages"])
-                    else:
-                        print("{:*^80}".format(" OK response "))
+                ctrl = prompt_from_body(body)
+                handle_action(s, ctrl)
 
-            if href != ctrl["href"]:
-                prev_href = href
-                href = ctrl["href"]
+                if href != ctrl["href"]:
+                    prev_href = href
+                    href = ctrl["href"]
+    except KeyboardInterrupt:
+        pass
